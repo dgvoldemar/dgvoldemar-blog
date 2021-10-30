8f8899ef4794cc04b316410c16b6b9db9afc185c
dgvoldemar <dgvoldemar@yandex.ru>
2021-10-30 02:01:20 -0700

    #3 Fix after review

--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -3,9 +3,10 @@
 <article
     class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
     <div class="py-6 px-5 lg:flex">
-        @includeWhen($post->thumbnail,'components.img-storage', ['src_suffix' => $post->thumbnail,
-                                                                'div_class' => 'flex-1 lg:mr-8',
-                                                                'img_class' => 'rounded-xl'])
+        @includeWhen(
+         $post->thumbnail,
+         'components.img-storage',
+         ['src_suffix' => $post->thumbnail, 'div_class' => 'flex-1 lg:mr-8', 'img_class' => 'rounded-xl'])
 
         <div class="flex-1 flex flex-col justify-between">
             <header class="mt-8 lg:mt-0">
